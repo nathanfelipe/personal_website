30cc0ff002902217b3d99f2cacaec8c655008c27
Nathan <felipennathan@gmail.com>
2026-04-15 02:54:29 +0200

    Update deleting of Heliophysics.tsx

--- src/pages/Heliophysics.tsx
+++ src/pages/Heliophysics.tsx
@@ -1,1 +1,5 @@
+import { useEffect, useRef, useState, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
+export default Heliophysics;
